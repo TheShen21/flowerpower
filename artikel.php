d6ddb1b2f14e2b9f2e1ea03410919d28539e10e9
TheShen21 <bshen@roc-dev.com>
2023-03-21 13:37:58 +0100

    Update artikel.php

--- artikel.php
+++ artikel.php
@@ -1,18 +1,6 @@
 <?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'flowepower');
-
-
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, 3308);
-
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-
-
-}
+require_once "backend/config.php";
 
 $name = $_POST["name"];
 $opmerking = $_POST["opmerking"];
