8d31220c7d691fa942e3dd8f7ba07ad0488c3c00
TheShen21 <bshen@roc-dev.com>
2023-03-28 14:10:03 +0200

    login system

---
+++ logged.php
@@ -0,0 +1,15 @@
+<?php
+echo "gelukt";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<a href="logout.php">logout</a>
+</body>
+</html>
